adb84bbff6531f2d9815065f0994c8f5530de5f5
Aung Khant Kyaw <aungkhantkyaw8918.com>
2025-05-02 23:01:39 +0630

    Initial Commit

---
+++ CardWorkout-Programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Digital Base on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CardWorkout-ProgrammaticTests/CardWorkout_ProgrammaticTests.swift
@@ -0,0 +1,17 @@
+//
+//  CardWorkout_ProgrammaticTests.swift
+//  CardWorkout-ProgrammaticTests
+//
+//  Created by Digital Base on 2/5/25.
+//
+
+import Testing
+@testable import CardWorkout_Programmatic
+
+struct CardWorkout_ProgrammaticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
